d31ade6a36f7968065996dc010d3fde386e48324
mario-barrientos-dev <92326524+mario-barrientos-dev@users.noreply.github.com>
2023-08-06 20:14:05 +0000

    Adding Dynamic Data

--- app.py
+++ app.py
@@ -1,12 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    "id":
+    1,
+    "title":
+    "Data Analist",
+    "location":
+    "Bogota, Colombia",
+    "description":
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere mattis dui sit amet ullamcorper. Cras vitae vulputate nisl, eu.",
+  },
+  {
+    "id":
+    2,
+    "title":
+    "ML Engineer",
+    "location":
+    "Bucaramanga, Colombia",
+    "description":
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere mattis dui sit amet ullamcorper. Cras vitae vulputate nisl, eu.",
+  },
+  {
+    "id":
+    3,
+    "title":
+    "Backend Developer",
+    "location":
+    "Medellin, Colombia",
+    "description":
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere mattis dui sit amet ullamcorper. Cras vitae vulputate nisl, eu.",
+  },
+  {
+    "id":
+    4,
+    "title":
+    "FullStack Developer",
+    "location":
+    "Pasto, Colombia",
+    "description":
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla posuere mattis dui sit amet ullamcorper. Cras vitae vulputate nisl, eu.",
+  },
+]
 
 
 @app.route("/")
 def helloWord():
-  greeting = "Hola Mundo"
-  return render_template("home.html", context=greeting)
+  # greeting = "Hola Mundo"
+  return render_template("home.html", jobs=JOBS, company_name="Slytherin")
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   app.run(
