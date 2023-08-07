faccfb5af9640b42a5f2347cbc1f20118ca4ea76
mario-barrientos-dev <92326524+mario-barrientos-dev@users.noreply.github.com>
2023-08-07 15:05:08 +0000

    Test new path

--- app.py
+++ app.py
@@ -1,23 +1,7 @@
 from flask import Flask, render_template, jsonify
-from sqlalchemy import text
-from database import engine
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
-
-
-def load_jobs_from_db():
-  with engine.connect() as conn:
-    results = conn.execute(text("select * from jobs"))
-    column_names = results.keys()  # Obtener los nombres de las columnas
-    jobs = []
-
-    for row in results.all():
-      result_dict = dict(
-        zip(column_names,
-            row))  # Combinar nombres de columna con valores de la fila
-      jobs.append(result_dict)
-
-    return jobs
 
 
 @app.route("/")
@@ -30,6 +14,13 @@ def helloWord():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  print(job)
+  return render_template("jobpage.html", job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 import os
+
 url = os.environ["DB_CONNECTION_STRING"]
 engine = create_engine(url,
                        connect_args={"ssl": {
@@ -18,6 +19,20 @@ def load_jobs_from_db():
         zip(column_names,
             row))  # Combinar nombres de columna con valores de la fila
       jobs.append(result_dict)
-    print(jobs)
 
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    results = conn.execute(text(f"select * from jobs where id = {id}"))
+    column_names = results.keys()  # Obtener los nombres de las columnas
+    jobs = []
+
+    for row in results.all():
+      result_dict = dict(
+        zip(column_names,
+            row))  # Combinar nombres de columna con valores de la fila
+      jobs.append(result_dict)
+
+    return jobs[0]
